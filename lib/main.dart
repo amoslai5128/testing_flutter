67c86b59dba743f606b4567605469980acfb7476
AmosLai <alexlai5128@gmail.com>
2021-09-16 23:09:22 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import './local_storage.dart';
+
+import 'package:flutter/material.dart';
+import 'package:states_rebuilder/states_rebuilder.dart';
+
+import 'state.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    RM.storageInitializer(SharedPreferencesStore());
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo CRUD Persistence'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key? key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                  onPressed: () async {
+                    // Fetch data from CRUD (Online)
+                    await testOrderCRUD.crud.read();
+                  },
+                  child: const Text('Fetch Online Data ')),
+              ElevatedButton(
+                  onPressed: () {
+                    // Refresh the state, then it'll auto-query back the cache data from localDB
+                    testOrderCRUD.refresh();
+                  },
+                  child: const Text('Refresh & Query from LocalDB')),
+              ElevatedButton(
+                  onPressed: () {
+                    // Delete the cache from localDB
+                    testOrderCRUD.deletePersistState();
+                  },
+                  child: const Text('Delete Cache')),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: OnCRUDBuilder(
+                  listenTo: testOrderCRUD,
+                  onWaiting: () => const Center(child: CircularProgressIndicator()),
+                  onResult: (_) {
+                    final List<ListTile> listOfResult = testOrderCRUD.state
+                        .map((order) =>
+                            ListTile(title: Text(order?.id ?? 'NO ID'), subtitle: Text(order?.orderName ?? 'NO NAME')))
+                        .toList();
+                    return Column(children: listOfResult);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/state.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+
+import 'package:states_rebuilder/states_rebuilder.dart';
+import 'package:testing_flutter/local_storage.dart';
+
+/* ------------------------------- Data Model ------------------------------- */
+class Order {
+  String id;
+  String orderName;
+  Order({
+    required this.id,
+    required this.orderName,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'orderName': orderName,
+    };
+  }
+
+  factory Order.fromMap(Map<String, dynamic> map) {
+    return Order(
+      id: map['id'] ?? '',
+      orderName: map['orderName'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Order.fromJson(String source) => Order.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Order(id: $id, orderName: $orderName)';
+}
+
+/* ------------------------ Repository With CRUD API ------------------------ */
+class OrderRepository implements ICRUD<Order, String> {
+  @override
+  Future<List<Order>> read(String? param) async {
+    print('READ');
+
+    await Future.delayed(Duration(seconds: 2)); // Fake latency
+    final fakeOrders =
+        List.generate(5, (index) => Order(id: 'id-$index', orderName: 'Order No.#$index')); // Generated fake orders
+
+    return fakeOrders;
+  }
+
+  @override
+  Future<Order> create(item, param) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  Future delete(List items, param) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  Future<void> init() async {}
+
+  @override
+  Future update(List items, param) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+}
+
+/* ----------------------------- Injected State ----------------------------- */
+final testOrderCRUD = RM.injectCRUD<Order?, String>(
+  () => OrderRepository(),
+  readOnInitialization: true, // NOTE Must add this line.
+  persist: () => PersistState(
+    key: '__Order__',
+    persistStateProvider: SharedPreferencesStore()..init(),
+    toJson: (List<Order?> orders) {
+      print('TO JSON');
+      final mappedOrders = (orders).map((o) => o?.toMap()).toList();
+      return jsonEncode(mappedOrders);
+    },
+    fromJson: (json) {
+      print('FROM JSON');
+      return (jsonDecode(json) as List<Map<String, dynamic>>).map((mappedOrder) => Order.fromMap(mappedOrder)).toList();
+    },
+  ),
+);
